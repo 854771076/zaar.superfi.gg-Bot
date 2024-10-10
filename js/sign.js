c8ed600bfd4904ca92430d83b47bb0504f3cc523
fuyang <fuyang854771076@gmail.com>
2024-10-10 20:39:39 +0800

    init

--- js/sign.js
+++ js/sign.js
@@ -8,14 +8,7 @@ async function sign(wif,message,type='bip322-simple') {
     return data
 
 }
-// AUAZgZl52s6am84kslAxge/4kzXcDuHHnE+bQuaAxfBOxKNQCw8S8S/7ZoYnrhP4mdSFJWD8Mu6lmVCuhJlCDEhw
-// AUAZgZl52s6am84kslAxge/4kzXcDuHHnE+bQuaAxfBOxKNQCw8S8S/
 
-// var e =sign('KxzuJfKMXrX8hK1JwPCcDimME3NcHZ5g9sTWFSfEjSLGYkZEuHk7','Address:\nbc1pt5y373ekqsuck0y8usell37dy8lms770lrrhg0x7s88288vz09eqa825ua\n\nNonce:\n42605c40a7795a853f274e1c9b48c0e05e5305a5457f1416a37dafaeafe7d19a\n\n')
-// e.then((result) => {
-//         console.log(result);  // 输出结果
-//         process.exit(0);      // 正常退出
-// })
 // 从命令行获取参数
 const wif = process.argv[2];
 const message = process.argv[3];
